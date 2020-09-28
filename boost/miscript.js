7a49939b452c7febdc3d163ee6954b5b5072be27
vjrincon <vjrincon@misena.edu.co>
2020-09-27 20:11:00 -0500

    varios
    ejemplo de carousel materialize y boostrap

---
+++ boost/miscript.js
@@ -0,0 +1,9 @@
+alert("buenas noches");
+document.write("¡Oh, gloria inmarcesible!!!!");
+document.write("<br>");
+document.write("¡Oh, júbilo inmortal!!");
+document.write("<br>");
+var num1=39,num2=83,suma;
+suma=num1+num2;
+document.write(suma);
+
